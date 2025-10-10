ce1e1dbbc38a35092037b086582849d0faff84e6
rozojavier17@gmail.com <sergiosslk27@gmail.com>
2025-10-10 11:21:40 -0400

    feat: initialize project with Astro and Tailwind CSS, add Songbar component
    - Updated package.json with project metadata and dependencies for Astro, Tailwind CSS, and Preact.
    - Removed unused components: Controls, Playercontainer, Progressbar, and Trackinfo.
    - Enhanced Tarjetamusica component with click handler for song playback.
    - Updated Layout.astro to include global styles.
    - Modified index.astro to render Tarjetamusica and include new Songbar component.
    - Added initial Songbar component and global CSS file for styling.

---
+++ src/components/Songbar.tsx
@@ -0,0 +1,7 @@
+export default function () {
+    return (
+        <div className="px-20 py-8 bg-neutral-700">
+      esta es mi barra de sonido
+        </div>
+    )
+}
--- src/components/Tarjetamusica.tsx
+++ src/components/Tarjetamusica.tsx
@@ -2,11 +2,22 @@ import { Song } from "../types/song"
 
 
 export default function (props: props) {
+    
+    // AQUI VOY A ESCRIBIR LA LOGICA DE COMO VA A FUNCIONAR
+    
+    
+    function handlerclick () {
+        alert(`Reproduciendo: ${props.cancion.title} - ${props.cancion.author}`);
+    }
+    
+    
+    
     return (
-    <div className="flex items-center">
-        <img  className="w-26 rounded" src="{props.cancion.imagen.url" alt="" />
+    <div className="flex items-center gap-4"> 
+        <img  className="w-26 rounded-full" src={props.cancion.image.url} alt="" />
         <div>
-            <h3>{props.cancion.title}</h3>
+            <h3 className="text-lg font-semibold">
+            {props.cancion.title}</h3>
             <p>{props.cancion.author}</p>
         </div>
     </div>
