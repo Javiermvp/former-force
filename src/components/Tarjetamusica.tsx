81c6208530695a694f410dafdaa57e3fc5453344
javier rozo <rozojavier17@gmail.com>
2025-10-13 15:26:10 -0400

    barra de sonido

--- src/components/Tarjetamusica.tsx
+++ src/components/Tarjetamusica.tsx
@@ -1,57 +1,46 @@
+import React from 'react';
+import { Song } from '../types/song';
+import { FaVolumeUp, FaVolumeMute } from 'react-icons/fa';
+import { useState } from 'react';
+import { IconContext } from 'react-icons';
+import Songbar from '../components/Songbar';
+import axios from 'axios';
 
-import { Song } from "../types/song"
-import { FaPlayCircle } from 'react-icons/fa';
-import React from "react";
-import { Axios } from "axios";
-import  tarjetamusica  from "../components/Tarjetamusica";
 
-    interface song  {
-    title: string;
-    author: string;
-    image: {
-        url: string;
-    }
+const canciones  =  await  axios.get ("https://api.institutoalfa.org/api/songs")
+
+interface Props {
+    cancion: Song;  
 }
 
-interface Props  {
-    cancion: song;
-};
+export default function Barradesonido(Props: Props) {
+    const [isMuted, setIsMuted] = useState(false);
 
+    const toggleMute = () => {
+        setIsMuted(!isMuted);
+    };
 
-
-export default function Tarjetamusica(Props: Props) {
-    const handleclick = () => {
-        alert(`Reproduciendo: $Props.cancion.title} - ${Props.cancion.author}`);
-    }
-    
     return (
-    <div className="flex items-center gap-4 relative p-4 bg-gray-800 rounded-lg hover:bg-gray-700 cursor-pointer"
-        onClick={handleclick}
-    >
-        <img className="w-26 rounded-full"
-        src={Props.cancion.image.url}
-        alt={Props.cancion.title}
-        />
-        <div className="flex flex-col">
-        <h3 className="text-white font-semibold">{Props.cancion.title}</h3>
-        <p className="text-gray-400">{Props.cancion.author}</p>
+        <div className="fixed bottom-0 left-0 right-0 bg-gray-900 text-white p-4 flex items-center justify-between">
+            <div className="flex items-center gap-4">
+                <img className="w-12 h-12 rounded-full"
+                    src={Props.cancion.image.url}
+                    alt={Props.cancion.title}
+                />
+                <div className="flex flex-col">
+                    <h3 className="text-white font-semibold">{Props.cancion.title}</h3>
+                    <p className="text-gray-400">{Props.cancion.author}</p>
+                </div>
+            </div>
+            <div className="flex items-center gap-4">
+                <IconContext.Provider value={{ size: '1.5em' }}>
+                    {isMuted ? (
+                        <FaVolumeMute onClick={toggleMute} />
+                    ) : (
+                        <FaVolumeUp onClick={toggleMute} />
+                    )}
+                </IconContext.Provider>
+            </div>
         </div>
-        
-        
-        <div className="text-black">
-        <FaPlayCircle size={24} className="hover:text-white" />
-        </div>
-    </div>
-)
+    );
 }
- 
-
-
-interface props {
-    cancion: Song;
-    
-     }
-
-     <style>
-
-     </style>
