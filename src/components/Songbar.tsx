8c744759f25b9a879d4ca70cf0f69a0a7c65c0f7
javier rozo <rozojavier17@gmail.com>
2025-10-13 14:05:52 -0400

    ESTA ES MI BARRA DE MUSICA

--- src/components/Songbar.tsx
+++ src/components/Songbar.tsx
@@ -1,7 +1,7 @@
 export default function () {
     return (
         <div className="px-20 py-8 bg-slate-700">
-      esta es mi barra de sonido
+      ESTA ES MI BARRA DE SONIDO
         </div>
     )
 }
