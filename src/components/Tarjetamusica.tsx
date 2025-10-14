a5c65aba3e65a93918c16569c98d1b48f3783b73
javier rozo <rozojavier17@gmail.com>
2025-10-14 14:10:39 -0400

    modificacion tarjetamusica.tsx

--- src/components/Tarjetamusica.tsx
+++ src/components/Tarjetamusica.tsx
@@ -1,39 +1,35 @@
 import React from 'react';
-import { Song } from '../types/song';
-import onclick from 'react';
-import { Props } from 'astro';
-import { useState } from 'react';
 import { FaPlayCircle } from 'react-icons/fa';
-import { type } from 'os';
+import type { Song } from '../types/song';
+
+interface Props {
+    cancion: Song
+}
 
 
-
-
-export default function tarjetamusica(Props: Song) {
+export default function tarjetamusica(props: Props) {
     const handerclick = () => {
-alert ("reproduciendo" + Props.cancion.Title);
+        alert("reproduciendo" + props.cancion.title);
     }
 
 
     return (
-        <div className='bg-gray-800 p-4 rounded-lg shadow-md flex items-center space-x-4 hover:bg-gray-700 cursor-pointer'
-        onClick={handerclick}>
+        <div className='bg-gray-800 p-4 rounded-lg shadow-md flex items-center space-x-4 hover:bg-gray-700 cursor-pointer' onClick={handerclick}>
 
-            <img className='w-26 rounded-full 
-             src={Props.cancion.image.url} 
-             alt={Props.cancion.Title}' 
+            <img className='w-26 rounded-full' 
+             src={props.cancion.image.url} 
+             alt={props.cancion.title} 
              />
              <div className='flex-1'>
-                <h3 className='text-white text-lg font-semibold>{cancion.Title}</h3>
-                 {Props.cancion.Title}</h3>
-                <p className="text-gray-400">{Props.cancion.author}</p>
+                <h3 className='text-white text-lg font-semibold'>{props.cancion.title}</h3>
+                <p className="text-gray-400">{props.cancion.author}</p>
              </div>
         
         
              <div className='text-clack-500'> 
                 <FaPlayCircle size={30} className='hover:text-white'/>
-        </div>
-        </div>
+            </div>
+        </div >
     )
 
 }
