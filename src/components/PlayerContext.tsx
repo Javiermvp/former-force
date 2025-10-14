7b741b560eabbf5998e470de31615a056f8edf40
javier rozo <rozojavier17@gmail.com>
2025-10-14 15:43:15 -0400

    s

--- src/components/PlayerContext.tsx
+++ src/components/PlayerContext.tsx
@@ -2,19 +2,31 @@ import React, { createContext, useState, useContext } from 'react';
 import type { Song } from '../types/song';
 
 interface PlayerContextType {
+  songs: Song
   currentSong: Song | null;
+  currentIndex: number
   isPlaying: boolean;
+  setPlaylist: (Song: Song) => void;
   playSong: (song: Song) => void;
   togglePlay: () => void;
+  playNext: () => void;
+  playPrev: () => void;
 }
 
 const PlayerContext = createContext<PlayerContextType | undefined>(undefined);
 
 export const PlayerProvider = ({ children }: { children: React.ReactNode }) => {
+  const [songs, setsongs] = useState<Song[]> ([]);
+  const [currentIndex, setcurrentIndex] =useState (0)
   const [currentSong, setCurrentSong] = useState<Song | null>(null);
   const [isPlaying, setIsPlaying] = useState(false);
 
+  const setPlaylist =(songs: Song[]) => {
+    setsongs (songs);
+  }
+
   const playSong = (song: Song) => {
+    const Index = songs.findIndex ((s) => s.id === song.id);
     setCurrentSong(song);
     setIsPlaying(true);
   };
