ebd3012ba8c6590bb30ecb9abdd4300b58212e45
Cercs <sergiosslk27@gmail.com>
2025-10-15 10:33:34 -0400

    refactor: update Songbar and Tarjetamusica components to use useSong hook for song data

--- src/components/Songbar.tsx
+++ src/components/Songbar.tsx
@@ -10,53 +10,53 @@ import { BiDownload } from 'react-icons/bi';
 import { BiHeart } from 'react-icons/bi';
 import { BiSolidHeart } from 'react-icons/bi';
 import { BiShareAlt } from 'react-icons/bi';
+import { useSong } from "../store";
 
 
 
-export default function (props: Props) {
+export default function () {
+    const {  song } = useSong() 
 
     return (
         <div className="Main-container">
-            
-            
-            <img src={props.song.image.url} alt={props.song.title}
+
+
+            <img src={song?.image.url} alt=""
                 style={
-                        {height:'90%',
-                        width:'auto',
+                    {
+                        height: '90%',
+                        width: 'auto',
                         objectFit: 'cover',
                         paddingLeft: '10px',
                         paddingTop: '10px',
-                        borderRadius: '20px'}
-                        } />
-            
+                        borderRadius: '20px'
+                    }
+                } 
+ />
 
             <div className="Second-cont">
-                <div className="Third-cont">
-                    <div className="Song-title" style={{paddingLeft: '10px'}}>
-                        <h1 style={{fontSize: 'clamp(16px, 5vw, 40px)'}}>{props.song.title}</h1>
-                        <p style={{fontSize: 'clamp(14px, 1vw, 40px)'}} >{props.song.author}</p>
+                <div className="Controls-cont">
+                    <div className="Song-title" style={{ paddingLeft: '10px' }}>
+                        <h1 style={{ fontSize: 'clamp(16px, 5vw, 40px)' }}>{song.title}</h1>
+                        <p style={{ fontSize: 'clamp(14px, 1vw, 40px)' }} >{song.author}</p>
                     </div>
 
                     <div className="Card-panel">
-                        <button> <BiDownload style={{fontSize: '20px', color: 'blue'}}/> </button>
-                        <button> <BiSkipPrevious style={{fontSize: '35px', color: 'blue'}}/> </button>
-                        <button> <FaPlay style={{fontSize: '25px', color: 'blue'}} /> </button>
-                        <button> <BiSkipNext style={{fontSize: '35px', color: 'blue'}} /> </button>
-                        <button> <BiShuffle style={{fontSize: '20px', color: 'blue'}} /> </button>
+                        <button> <BiDownload style={{ fontSize: '20px', color: 'blue' }} /> </button>
+                        <button> <BiSkipPrevious style={{ fontSize: '35px', color: 'blue' }} /> </button>
+                        <button> <FaPlay style={{ fontSize: '25px', color: 'blue' }} /> </button>
+                        <button> <BiSkipNext style={{ fontSize: '35px', color: 'blue' }} /> </button>
+                        <button> <BiShuffle style={{ fontSize: '20px', color: 'blue' }} /> </button>
                     </div>
                 </div>
 
                 <div className="Social-buttons">
-                    <button> <BiHeart style={{fontSize: '24px', color: 'blue', paddingTop: '10px'}} /> </button>
-                    <button> <BiShareAlt style={{fontSize: '22px', color: 'blue', paddingBottom: '10px'}} /> </button>
+                    <button> <BiHeart style={{ fontSize: '24px', color: 'blue', paddingTop: '10px' }} /> </button>
+                    <button> <BiShareAlt style={{ fontSize: '22px', color: 'blue', paddingBottom: '10px' }} /> </button>
                 </div>
             </div>
 
         </div>
     )
 
-}
-
-interface Props {
-    song: Song
 }
--- src/components/Tarjetamusica.tsx
+++ src/components/Tarjetamusica.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FaPlayCircle } from 'react-icons/fa';
 import type { Song } from '../types/song';
+import { useSong } from '../store';
 
 
 interface Props {
@@ -11,26 +12,30 @@ interface Props {
 export default function tarjetamusica(props: Props) {
     const handerclick = () => {
         alert("reproduciendo" + props.cancion.title);
-    }
-    
+        updateSong(props.cancion)
+     }
+
+     const {updateSong} = useSong()
+
+
 
 
 
     return (
         <div className='bg-gray-800 p-4 rounded-lg shadow-md flex items-center space-x-4 hover:bg-gray-700 cursor-pointer' onClick={handerclick}>
 
-            <img className='w-26 rounded-full' 
-             src={props.cancion.image.url} 
-             alt={props.cancion.title} 
-             />
-             <div className='flex-1'>
+            <img className='w-26 rounded-full'
+                src={props.cancion.image.url}
+                alt={props.cancion.title}
+            />
+            <div className='flex-1'>
                 <h3 className='text-white text-lg font-semibold'>{props.cancion.title}</h3>
                 <p className="text-gray-400">{props.cancion.author}</p>
-             </div>
-        
-        
-             <div className='text-clack-500'> 
-                <FaPlayCircle size={30} className='hover:text-white'/>
+            </div>
+
+
+            <div className='text-clack-500'>
+                <FaPlayCircle size={30} className='hover:text-white' />
             </div>
         </div>
     )
